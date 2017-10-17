b0c26ffc8ade7e2de696d6660f2ff9b4ec689237
adeputras <poetrajerami@gmail.com>
2017-10-17 21:00:59 +0700

    oke

--- js/scripts.js
+++ js/scripts.js
@@ -12648,7 +12648,10 @@ if (typeof jQuery === 'undefined') {
       setTimeout(function(){
         $('.form--search').addClass('search-open');
        }, 50);
-      
+      if($('.menu--mobile--toggle ._toggle').hasClass('open')){
+        $('.menu--mobile--toggle ._toggle').removeClass('open');
+        $('.menu--mobile--main').removeClass('open');
+      }
       $('.menu--mobile--toggle ._toggle').addClass('search-open');
     });
     
